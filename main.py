1d873d862afeec43906212e8df904f5c211ff1e5
universalmind303 <cory.grinstead@gmail.com>
2025-03-12 10:51:40 -0700

    fix file path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from daft import col
 
 # hack, we don't actually have a way to read in a text file,
 # so i'm using the csv reader and using a delimiter that is unlikely to be used so it reads each line as a column
-df = daft.read_csv('./sample.text', delimiter='¦', has_headers=False).select(col("column_1").alias("text"))
+df = daft.read_csv('./data/sample.text', delimiter='¦', has_headers=False).select(col("column_1").alias("text"))
 # tokenize_encode isnt showing up in docstrings
 df = df.select(col("text").str.tokenize_encode("cl100k_base")).collect()
 # shouldn't tokenize_encode return a fixedsizelist instead of a list?
