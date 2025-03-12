f545b3a86d86a016de4e5d0ec19bc840f63187f8
R. Conner Howell <robert@eventualcomputing.com>
2025-03-12 11:15:13 -0700

    creates scripts repo

---
+++ scripts/tokenizer.py
@@ -0,0 +1,21 @@
+# %%
+import daft
+import tiktoken
+from daft import col
+
+# hack, we don't actually have a way to read in a text file,
+# so i'm using the csv reader and using a delimiter that is unlikely to be used so it reads each line as a column
+df = daft.read_csv("./data/sample.text", delimiter="¦", has_headers=False).select(col("column_1").alias("text"))
+# tokenize_encode isnt showing up in docstrings
+# df = df.select(col("text").str.tokenize_encode("cl100k_base")).collect()
+# shouldn't tokenize_encode return a fixedsizelist instead of a list?
+
+
+@daft.udf(return_dtype=daft.DataType.list(daft.DataType.int32()))
+def tokenize(text: daft.Series, encoder: str):
+    enc = tiktoken.get_encoding(encoder)
+    items = [enc.encode(t) for t in text.to_pylist()]
+    return items
+
+
+df.select(tokenize(col("text"), encoder="o200k_base")).show()
