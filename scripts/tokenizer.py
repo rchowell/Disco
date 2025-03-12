711d68c9c862b0d0fce89bafa6acee3ad42f686a
universalmind303 <cory.grinstead@gmail.com>
2025-03-12 11:50:07 -0700

    create base tokenizer class

--- scripts/tokenizer.py
+++ scripts/tokenizer.py
@@ -1,21 +1,28 @@
 # %%
 import daft
+import disco
+from disco import Disco
 import tiktoken
 from daft import col
 
-# hack, we don't actually have a way to read in a text file,
-# so i'm using the csv reader and using a delimiter that is unlikely to be used so it reads each line as a column
-df = daft.read_csv("./data/sample.text", delimiter="¦", has_headers=False).select(col("column_1").alias("text"))
-# tokenize_encode isnt showing up in docstrings
-# df = df.select(col("text").str.tokenize_encode("cl100k_base")).collect()
-# shouldn't tokenize_encode return a fixedsizelist instead of a list?
+# todo: add a way to register a tokenizer using a decorator
+# @disco.register("my_tokenizer")
+class MyTokenizer(disco.Tokenizer):
+    def __init__(self, id: str):
+        self.id = id
+        self.encoder = tiktoken.get_encoding("o200k_base")
+
+    def encode(self, data: str) -> list:
+        return self.encoder.encode(data)
+
+    def decode(self, data: list) -> str:
+        return self.encoder.decode(data)
 
 
-@daft.udf(return_dtype=daft.DataType.list(daft.DataType.int32()))
-def tokenize(text: daft.Series, encoder: str):
-    enc = tiktoken.get_encoding(encoder)
-    items = [enc.encode(t) for t in text.to_pylist()]
-    return items
+# example usage
+# cat = Disco()
+# cat.put_object("my_tokenizer", MyTokenizer)
 
+# cat.put_volume("data", "./data")
+# df = disco.read("data://sample.text").transform("my_tokenizer")
 
-df.select(tokenize(col("text"), encoder="o200k_base")).show()
